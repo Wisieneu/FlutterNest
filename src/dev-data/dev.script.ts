455c22e8e2fa9c33183669e716fc8ea3a17601ef
wisie <wisnia501@gmail.com>
2023-09-27 15:40:24 +0200

    add dev-data kit initial

---
+++ src/dev-data/dev.script.ts
@@ -0,0 +1,24 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   //   const user = await prisma.user.create({
+//   //     data: {
+//   //       username: 'testuser1',
+//   //       email: 'testuser1@example.com',
+//   //     },
+//   //   });
+//   const user = await prisma.user.deleteMany({});
+//   console.log(user);
+// }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
