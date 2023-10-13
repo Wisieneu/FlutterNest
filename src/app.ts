fab41dfdc56e468c78f3634a1d32e46630203204
wisie <wisnia501@gmail.com>
2023-10-13 13:12:51 +0200

    add dev api error handler

--- src/app.ts
+++ src/app.ts
@@ -9,29 +9,22 @@ import helmet from 'helmet';
 import userRouter from './routes/user.routes';
 import tweetRouter from './routes/tweet.routes';
 import viewRouter from './routes/view.routes';
-import initDb from './config/database';
-// import { PrismaClient } from '@prisma/client';
+import globalErrorHandler from './controllers/error.controller';
 
 class App {
   public app: Application;
-  public db: any;
 
   constructor() {
     this.app = express();
-    this.db = this.initializeDatabaseConnection();
     this.initializeMiddlewarePlugins();
     this.attachRoutes();
-  }
-
-  protected initializeDatabaseConnection() {
-    return initDb();
   }
 
   protected initializeMiddlewarePlugins(): void {
     this.app.use(
       cors({
         credentials: true,
-      })
+      }),
     );
     this.app.use(helmet());
     this.app.use(cookieParser());
@@ -45,6 +38,7 @@ class App {
     this.app.use('/api/v1/users', userRouter);
     this.app.use('/api/v1/tweets', tweetRouter);
     this.app.use('/', viewRouter);
+    this.app.use(globalErrorHandler);
   }
 }
 
