089346bff3fd004df8ccec0e0b2c41d606128af2
wisie <wisnia501@gmail.com>
2023-10-15 02:30:14 +0200

    implement dev error API handling

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -2,7 +2,28 @@ import { NextFunction, Request, Response } from 'express';
 import AppError from '../utils/appError';
 
 const sendDevError = (err: AppError, req: Request, res: Response) => {
+  // API error handling
   console.error(err);
+  if (req.originalUrl.startsWith('/api')) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  } else {
+    // Rendered website error handling
+    // change to render() later on TODO:
+    res.status(err.statusCode).json({
+      title: 'Error',
+      msg: err.message,
+    });
+  }
+};
+
+//TODO:
+const sendProdError = (err: AppError, req: Request, res: Response) => {
+  // API error handling
   if (req.originalUrl.startsWith('/api')) {
     if (err.isOperational) {
       res.status(err.statusCode).json({
@@ -18,16 +39,10 @@ const sendDevError = (err: AppError, req: Request, res: Response) => {
       });
     }
   }
-};
-
-//TODO:
-const sendProdError = (err: AppError, req: Request, res: Response) => {
-  console.error(err);
-  if (req.originalUrl.startsWith('/api')) {
-    res.status(err.statusCode).json({
-      status: 'error',
-      message: 'An unknown error has occurred. Please try again later.',
-    });
+  if (err.isOperational) {
+    res.status(err.statusCode).send(`${err}`);
+  } else {
+    res.status(err.statusCode).send(`${err}`);
   }
 };
 
