3a9869a1073fc122a72d2feafe2ef7401de53e23
wisie <wisnia501@gmail.com>
2023-10-20 02:35:02 +0200

    extend user schema with signUp method

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,17 +1,49 @@
 import { PrismaClient } from '@prisma/client';
+import { Request } from 'express';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import AppError from '../utils/appError';
 
 dotenv.config({ path: './.env' });
 
-const initializeDatabaseConnection = async () => {
-  let db;
-  try {
-    db = new PrismaClient();
-    console.log(`✅ PostgreSQL database connection has been established successfully.`);
-  } catch (err) {
-    console.log(`❌ Could not connect to the PostgreSQL database: ${err}`);
-  }
-  return db;
-};
+export const database = new PrismaClient().$extends({
+  model: {
+    user: {
+      async signUp(
+        email: string,
+        username: string,
+        password: string,
+        passwordConfirm: string,
+      ) {
+        if (password !== passwordConfirm) {
+          throw new AppError('Provided passwords do not match.', 400);
+        }
+        const hash = await bcrypt.hash(password, 10);
+        const newUser = await database.user.create({
+          data: {
+            email,
+            username,
+            password: hash,
+            displayName: username,
+          },
+        });
+        return newUser;
+      },
 
-export default initializeDatabaseConnection;
+      async updatePassword(
+        password: string,
+        passwordConfirm: string,
+        req: Request,
+      ) {},
+
+      async checkPassword<User>(candidatePassword: string) {
+        // return await bcrypt.compare(candidatePassword, this.password);
+      },
+    },
+  },
+});
+
+// console.clear();
+console.log(
+  `\n✅\x1b[33m PostgreSQL database connection has been established successfully\x1b[0m`,
+);
