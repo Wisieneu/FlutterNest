ed68158b5d1afb18fe0f1fd7aabf882c85f75840
wisie <wisnia501@gmail.com>
2024-08-29 22:59:47 +0200

    add try/catch for migration process

--- backend/src/db/migrate.ts
+++ backend/src/db/migrate.ts
@@ -1,15 +1,19 @@
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { db, connection } from '.';
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { db, connection } from ".";
 
-import logger from '../utils/logger';
+import logger from "../utils/logger";
 
 async function main() {
   // const pool = new Pool({ connectionString: process.env.DB_URL });
   // const db: NodePgDatabase = drizzle(pool);
 
-  logger.info('[migrate] Running migrations...');
-  await migrate(db, { migrationsFolder: 'src/db/drizzle' });
-  logger.info('[migrate] All migrations have been ran, exiting...');
+  logger.info("[migrate] Running migrations...");
+  try {
+    await migrate(db, { migrationsFolder: "src/db/drizzle" });
+    logger.info("[migrate] All migrations have been ran, exiting...");
+  } catch (err) {
+    logger.error("[migrate] Migration failed", err);
+  }
 
   await connection.end();
 }
